235a07c78e108d78d2e745bb68d3c62dbf9edafa
Kuroce-Tepantongo <c.esteban.hernandezbr@gmail.com>
2024-12-19 15:42:04 -0600

    Delete environment carpet

--- environment/DefaultLambdaFuctTest/lambda_function.py
+++
@@ -1,8 +0,0 @@
-import json
-
-def lambda_handler(event, context):
-    # Hello from Cloud9
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
--- environment/Python basics/print.py
+++
@@ -1,5 +0,0 @@
-# print Strings
-print('Hello AWS')
-
-# Concatenate and print
-print('This is a string\n' + str(1996))
